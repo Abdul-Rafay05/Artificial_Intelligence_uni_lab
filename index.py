2d6648908247fccd164e74bc1b3a45c7b056a27b
Abdul-Rafay05 <syedabdulrafay163@gmail.com>
2024-10-23 10:41:52 +0500

    Shape # 4

--- index.py
+++ index.py
@@ -5,3 +5,11 @@ print("", "*" * 10, "\n", "*" * 10, "\n", "*" * 10, "\n", "*" * 10, "\n")
 # Shape # 2
 print("Rectangle Shape", "\n")
 print("", "*" * 17, "\n", "*" * 17, "\n", "*" * 17, "\n", "*" * 17, "\n")
+
+# Shape # 3
+print("Right triangle Shape", "\n")
+print("", "*" * 1, "\n", "*" * 2, "\n", "*" * 3, "\n", "*" * 4, "\n", "*" * 5, "\n")
+
+# Shape # 4
+print("triangle Shape", "\n")
+print("     ", "*" * 1, "\n", "   ", "* " * 2, "\n", "  ", "* " * 3, "\n", " ", "* " * 4, "\n", "", "* " * 5);
