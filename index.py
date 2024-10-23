08d6164ccd3a8859a04d0fee22b2d9aaa8dbe59a
Abdul-Rafay05 <syedabdulrafay163@gmail.com>
2024-10-23 10:26:28 +0500

    Shapes

---
+++ index.py
@@ -0,0 +1,13 @@
+# a = 1
+# b = "Abdul Rafay"
+# c = True
+# d = 1.20
+# e = 1 + 2j
+# f = None
+
+# print("Basic Data Type")
+# print(
+#     f"int: {a}, string: {b} ,Boolean: {c} , Float: {d} , complex: {e} ,Nonetype: {f} "
+# )
+
+print("", "*" * 10, "\n", "*" * 10, "\n", "*" * 10, "\n", "*" * 10)
